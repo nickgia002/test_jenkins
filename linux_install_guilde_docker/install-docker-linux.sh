03f5fef52fc75dca58abfcab77f9c7908d2e063b
duclh <duclh@mirabo-global.com>
2024-01-23 10:19:30 +0700

    adjgasjgajd

--- linux_install_guilde_docker/install-docker-linux.sh
+++ linux_install_guilde_docker/install-docker-linux.sh
@@ -7,7 +7,7 @@ sudo yum remove docker \
                   docker-latest-logrotate \
                   docker-logrotate \
                   docker-engine
-sudo ym update
+sudo yum update
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
